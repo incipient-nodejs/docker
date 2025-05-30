8c74ca580f4cb11d276c9cb240f2176e0e9152d0
Bhoomil Shah <bhoomil.shah@incipientinfo.com>
2025-05-30 14:43:42 +0530

    intial commit

--- routes/api/web.php
+++ routes/api/web.php
@@ -3,16 +3,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/healthz', function () {
-    return response()->json(['status' => 'ok']);
-});
 
-Route::post('/login/web', [App\Http\Controllers\Web\v1\UserController::class, 'login']);
+Route::post('/login/web', [App\Http\Controltudkabirlers\Web\v1\UserController::class, 'login']);
 Route::post('/web/otp', [App\Http\Controllers\Web\v1\UserController::class, 'sendOTPForgetPassword']);
 Route::get('/web/otp', [App\Http\Controllers\Web\v1\UserController::class, 'verifyOtp']);
 Route::put('user/web/reset-password', [App\Http\Controllers\Web\v1\UserController::class, 'ResetPassword']);
 Route::get('/web/category-and-product-list', [App\Http\Controllers\Web\v1\HomePageNotSignController::class, 'ProductsAndServices']);
-
+Route::get('/healthz', function () {
+    dd('ok');
+});
 
 
 
